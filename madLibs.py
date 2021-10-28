851748a34fba195ea71d146addbfcc75136a20c1
Zach Dailey <zachd19@gmail.com>
2021-10-28 03:29:09 -0500

    Adding Guess Number (PC) game as well as updated madLibs file.

---
+++ madLibs.py
@@ -0,0 +1,9 @@
+adj = input("Adjective: ")
+verb1 = input("Verb1: ")
+verb2 = input("Verb2: ")
+famous_person = input("Famous Person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because " \
+         f"I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
